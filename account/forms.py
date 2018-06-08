bc6a4f6103cdbc3ceb770c279f9cd0c9657f7feb
neeraj <shuklaneeraj2014.2015@gmail.com>
2018-06-08 20:22:57 +0530

    Email validation in registration form

--- account/forms.py
+++ account/forms.py
@@ -19,3 +19,12 @@ class UserRegisterForm(forms.ModelForm):
             'email2',
             'password'
         ]
+    def clean_email2(self):
+        email = self.cleaned_data.get('email')
+        email2 = self.cleaned_data.get('email2')
+        if email != email2:
+            raise forms.ValidationError("Emails must match")
+        email_qs = User.objects.filter(email=email)
+        if email_qs.exists():
+            raise forms.ValidationError("This email has already been registered")
+        return email
